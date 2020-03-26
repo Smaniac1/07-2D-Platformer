202027e177d95a284d5ad3e3a95efc97c39f85bd
Jason Francis <jarfranc@indiana.edu>
2020-03-25 22:20:05 -0400

    Saves and loads coins and enemies
    Sorry this took so long, it was a tricky problem, but I think I have a solution that works pretty well. When you turn it in, include a note that I should grant you four days of leniency.

--- World2.gd
+++ World2.gd
@@ -1,4 +1,21 @@
 extends Node
 
+onready var Global = get_node("/root/Global")
+
 func _ready():
-	get_node("/root/Global").level = 2
+	Global.level = 2
+	Global.init()
+	if Global.loaded:
+		Global.load_coins()
+		Global.load_enemies()
+		for c in Global.coins:
+			if Global.coins[c] == false:
+				var temp = get_node("Coins/%s" % c)
+				if temp:
+					temp.queue_free()
+		for e in Global.enemies:
+			if Global.enemies[e] == false:
+				var temp = get_node("Basic_Enemies/" + e)
+				if temp:
+					temp.queue_free()
+	Global.loaded = false
